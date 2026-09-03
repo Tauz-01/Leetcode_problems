4d06db643f15dfa7310a3bbded75acca50cfa869
Tauz-01 <tausifmohammed52@gmail.com>
2026-09-03 23:27:03 +0530

    Time: 5 ms (83.33%), Space: 122 MB (18.12%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int minodd = Integer.MAX_VALUE;
+        int mineven = Integer.MAX_VALUE;
+        for(int num : nums1) {
+             if(num%2 == 0 )
+                mineven = Math.min(mineven,num);
+             else
+                minodd = Math.min(minodd,num);
+        }
+        if(mineven == Integer.MAX_VALUE)
+           return true;
+        if(minodd == Integer.MAX_VALUE)
+           return true;   
+
+         return minodd<mineven;  
+
+        
+    }
+}
